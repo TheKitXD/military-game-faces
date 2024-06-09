b30a6487e60e22889163eae09a04a2d4bc322d8b
kiet.official <114701033+TheKitXD@users.noreply.github.com>
2024-06-09 19:40:46 -0400

    Create FacesSet.lua

---
+++ FacesSet.lua
@@ -0,0 +1,7 @@
+local module = {}
+module.Whitelist = {"KietMaples"}
+module.Faces = {"Left, face.", "Right, face.", "About, face.", "Center, face.", "Right, incline.", "Left, incline."}
+module.Activation = "FACE!"
+module.Delay = .15
+
+return module
